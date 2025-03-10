038f0597836f85e62a60ffa4cae8ec2ea3216ebc
Renato Willyan Moratto Filho <80411445+RenatoMoratto@users.noreply.github.com>
2025-03-10 21:46:20 +0000

    style: add scripts `lint:check` and `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Implementação do tabnews.com.br feito durante o https://curso.dev
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home(params) {
-    return <h1>Nath, eu amo você. Se você me ama, me dá um beijinho?</h1>
+  return <h1>Nath, eu amo você. Se você me ama, me dá um beijinho?</h1>;
 }
 
 export default Home;
